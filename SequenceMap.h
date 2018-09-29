46bc4f50c554b263dfe1652d0818be6f2e4affd9
Navid <salem.saboori00@myhunter.cuny.edu>
2018-09-29 19:49:57 -0400

    seq

--- SequenceMap.h
+++ SequenceMap.h
@@ -2,19 +2,27 @@
  * SequenceMap.h
  *
  *  Created on: Sep 27, 2018
- *      Author: sky2464
+ *      Author: Salem Saboori
  */
 
 #ifndef SEQUENCEMAP_H_
 #define SEQUENCEMAP_H_
 
+#include <vector>
+using namespace std;
+
+
 class SequenceMap {
-public:
-	SequenceMap();
-	virtual ~SequenceMap();
+  public:
+  	SequenceMap() =default;
+	SequenceMap(const SequenceMap & )= default; //Copy Constractur
+	SequenceMap(SequenceMap && )= default; //Move Constractur
+	SequenceMap & operator =(const SequenceMap & )= default; //Copy assignment operator
+	SequenceMap & operator =(SequenceMap && )= default; //Move assignment operator
+	~SequenceMap() = default; // Destractur
 
 
-private:
+  private:
 	string recognition_sequence_ ;
 	vector<string> enzyme_acronyms_;
 };
